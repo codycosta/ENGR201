fcaae1829dae4a3d4552aca36b0fdadc5a69c7b2
Cody Costa <123146937+codycosta@users.noreply.github.com>
2024-04-19 14:39:47 -0700

    Updated test.py with hw question for practice

--- test.py
+++ test.py
@@ -1,41 +1,67 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import time
-
-x = np.array([[0, 1, 2, 3], 
-              [9, 8, 7, 6]])
-
-# print(x[1,2])
-
-'''this is cool!'''
-
-# slope esitmator using numpy arrays
-
-def slope(lower, upper):
-
-    interval = np.array([lower, upper])
-    values = interval * 8 - 1.86 * interval**2
-
-    return np.round( (values[1] - values[0]) / (upper - lower), 2 )
-
-def time_function_execution(func):
-    start = time.time()
-    func
-    end = time.time()
-
-    return print(f'function executed in {end - start} seconds')
-
-# print(slope(1, 2))
-# time_function_execution(slope(1, 2))
+import math
 
 
-arr = np.array([[1, 2, 3], 
-                [4, 5, 6]])
+''' let's try to learn a bit of numpy here '''
 
-for x in arr:
-  for y in x:
-    print(y)
+# x = np.linspace(0, 2*np.pi, 100)
 
-x = x[x % 2 == 0]
-print(x)
+# plt.plot(x, np.sin(x), x, np.cos(x))
+# plt.xlabel('time')
+# plt.ylabel('sin(t)')
+# plt.title('My First Graph')
+# plt.legend(('sin(t)', 'cos(t)'))
+# plt.show()
+
+# y = np.array([[1, 2],
+#               [3, 4]])
+
+# z = np.array([[4, 3],
+#               [2, 1]])
+
+# calculates dot product
+# w = y @ z
+# print(w)
+
+# performs element-wise multiplication
+# print(y * z)
+
+
+''' 
+
+    probability hw qeustion, plotting distribution of n choose k:
+    
+    compute n choose k for n = 0 ... 6, 0 <= k <= n and plot to create pascal's triangle
+    plot the value of n choose k for each value of n
+
+'''
+
+# let's make the x axis k, and the y axis nchoosek, and we will include the value n in the legend as a denoter
+
+upper_limit = 6    # change this to edit the sample size of the graphs
+
+legends = []
+
+
+n = np.array([i for i in range(upper_limit + 1)])
+
+for num in n:
+
+    legends.append(f'n = {num}')
+
+    k = np.array([j for j in range(num+1)])
+    nchoosek = np.array([math.comb(num, el) for el in k])
+
+    plt.plot(k ,nchoosek, marker='o')
+
+
+# design our plot
+
+plt.xlabel('k')
+plt.ylabel('n choose k')
+plt.legend(legends)
+plt.title(f'Plots of comb(n, k) for values n,k <= {upper_limit}')
+
+plt.show()
