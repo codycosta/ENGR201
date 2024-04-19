3fd47365c359a79f16e56974cadc0e8856b94d92
Cody <costa.cody7@gmail.com>
2024-04-18 23:53:57 -0700

    more edits

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import time
 x = np.array([[0, 1, 2, 3], 
               [9, 8, 7, 6]])
 
-# print(x[1][2])
+# print(x[1,2])
 
 '''this is cool!'''
 
@@ -26,5 +26,16 @@ def time_function_execution(func):
 
     return print(f'function executed in {end - start} seconds')
 
-print(slope(1, 2))
-time_function_execution(slope(1, 2))
+# print(slope(1, 2))
+# time_function_execution(slope(1, 2))
+
+
+arr = np.array([[1, 2, 3], 
+                [4, 5, 6]])
+
+for x in arr:
+  for y in x:
+    print(y)
+
+x = x[x % 2 == 0]
+print(x)
