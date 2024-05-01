b33299022db4c3a73a20e5c1c842f6c264dd2580
Costa <Cody.Costa@kla-tencor.com>
2024-04-30 17:35:17 -0700

    added hw2q1b problem work, renamed files for organization

---
+++ probability-hw2-q1b.py
@@ -0,0 +1,28 @@
+def compute_n_terms(total_events:int) -> dict[str: any]:
+    '''
+        Auxiliary calculator for EVENTS problem 1b in Homework 2
+
+        return:
+        
+        calculates the number of terms in continuing the conversation joint probability expansion
+        given only one condition can be applied per term
+
+        n! terms exist for the regular expansion so this should yield more than that
+    '''
+
+    arr = np.array([1])
+
+    for i in range(2, total_events + 1):
+
+        arr = np.append(arr, math.comb(i, 2))
+        
+    # return np.sum(arr)
+    # return math.factorial(np.sum(arr))
+
+    return {
+        'terms': math.factorial(np.sum(arr)),
+        'large_enough': math.factorial(np.sum(arr)) > math.factorial(total_events)
+    }
+
+
+print(compute_n_terms(5))
--- test.py
+++ test.py
@@ -67,3 +67,33 @@ plt.show()
 
 
 w = [x for x in [0, 1, 2, 3, 4, 5, 6] if x % 2 == 0]
+
+
+def compute_n_terms(total_events:int) -> dict[str: any]:
+    '''
+        Auxiliary calculator for EVENTS problem 1b in Homework 2
+
+        return:
+
+        calculates the number of terms in continuing the conversation joint probability expansion
+        given only one condition can be applied per term
+
+        n! terms exist for the regular expansion so this should yield more than that
+    '''
+
+    arr = np.array([1])
+
+    for i in range(2, total_events + 1):
+
+        arr = np.append(arr, math.comb(i, 2))
+        
+    # return np.sum(arr)
+    # return math.factorial(np.sum(arr))
+
+    return {
+        'terms': math.factorial(np.sum(arr)),
+        'large_enough': math.factorial(np.sum(arr)) > math.factorial(total_events)
+    }
+
+
+print(compute_n_terms(5))
