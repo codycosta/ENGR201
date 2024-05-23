500402a3325bae318ee5031e7deb2bc881a3851d
Costa <Cody.Costa@kla-tencor.com>
2024-05-22 22:00:49 -0700

    commit... not sure what

--- midterm-practice.py
+++ midterm-practice.py
@@ -1,12 +1,33 @@
 
 import numpy as np
 import scipy
-import scipy.linalg
+import scipy.linalg as sl
 
-A: np.ndarray = np.array([[0, 1, 2],
-                          [2, 4, 4],
-                          [1, 1, 1]])
+A: np.ndarray = np.array([[1, 1, 1],
+                          [1, 4, 4],
+                          [1, 4, 8]])
 
-factorization = scipy.linalg.lu_factor(A)
+factorization = sl.lu_factor(A)
 
-print(factorization[0])
+# print(factorization[0])
+# print(factorization[1])
+# print(factorization)
+
+# print(factorization[0] @ factorization[0])
+
+I = np.eye(A.shape[0])
+# print(I)
+
+B: np.ndarray = np.array([[1, 2, 0, 1],
+                          [0, 1, 1, 0],
+                          [1, 2, 0, 1]])
+
+# print(sl.lu_factor(B))
+
+
+print(len(sl.lu(A)))
+print('Permutation:\n', sl.lu(A)[0], '\n')
+print('Lower:\n', sl.lu(A)[1], '\n')
+print('Upper:\n', sl.lu(A)[2], '\n')
+
+P, L, U = sl.lu(A)
