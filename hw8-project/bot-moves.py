0a90f4e6dd6bdc2fb1a211706ac8bd776c57388e
Cody <costa.cody7@gmail.com>
2024-06-08 16:29:15 -0700

    save hw8.ipynb as html

--- hw8-project/bot-moves.py
+++ hw8-project/bot-moves.py
@@ -1,3 +1,20 @@
 ''' script to intelligently make computer moves during tic tac toe games
     scales with difficulty '''
 
+import random
+
+def easy_computer_move():
+    ''' computer picks a random open space to place an X or O, no thinking needed '''
+
+    return random.randint(0,8)
+
+
+def medium_computer_move():
+    ''' computer thinks a little more about where to put their next move, can still be beat '''
+    ...
+
+
+def hard_computer_move():
+    ''' computer actively tries to win '''
+    ...
+
