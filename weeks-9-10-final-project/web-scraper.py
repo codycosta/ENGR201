64ae1e536294466f6fad107972cf9e15d5454a96
Costa <Cody.Costa@kla-tencor.com>
2024-06-18 16:05:19 -0700

    adjusted ordering of the notebook

--- weeks-9-10-final-project/web-scraper.py
+++ weeks-9-10-final-project/web-scraper.py
@@ -1,13 +1,24 @@
-
+import requests
+from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 url = 'https://nuforc.org/subndx/?id=all'
 
-page = urlopen(url)
+# page = urlopen(url)
 
-html = page.read().decode('utf-8')
+# html = page.read().decode('utf-8')
 
-table_start = html.find('<tbody>')
-table_end = html.find('</tbody>')
+# table_start = html.find('<tbody>')
+# table_end = html.find('</tbody>')
 
-print(html[table_start: table_end])
+# print(html[table_start: table_end])
+
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+headers = soup.find('thead')
+
+table_body = soup.find('tbody')
+
+stuff = table_body.find_all('tr')
+print(stuff)
