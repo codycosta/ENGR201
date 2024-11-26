155ab2d995db7e1dd3a31e3e13856ff70937192d
Cody <costa.cody7@gmail.com>
2024-11-26 01:08:49 -0800

    new project files

---
+++ markell-python/project/helper_functions.py
@@ -0,0 +1,122 @@
+# user defined functions for zoo.py
+
+import random
+import datetime
+
+def generate_zoo_population(POPULATION, ANIMALS, NAMES, PERSONALITY, DIET) -> dict:
+
+    Animals = {}
+
+    for k in range(POPULATION):
+        # choose random animal as index from list
+        animal_idx = random.randint(0, len(ANIMALS)-1)
+        species = ANIMALS[animal_idx]
+
+        # choose random name as index from list and remove it from the names array
+        name_idx = random.randint(0, len(NAMES)-1)
+        name = NAMES[name_idx]
+        NAMES.remove(name)
+
+        # choose random personality trait
+        personality_idx = random.randint(0, len(PERSONALITY)-1)
+        personality = PERSONALITY[personality_idx]
+
+        # choose random diet preference for each animal (results might not make sense in real life but whatever)
+        diet_idx = random.randint(0, len(DIET)-1)
+        diet = DIET[diet_idx]
+
+        Animals[name] = {
+            'species': species,
+            'age': random.randint(1, 50),
+            'personality': personality,
+            'diet': diet
+        }
+
+    return Animals
+
+
+def generate_health_data(animals, HEALTH) -> dict:
+    
+    health_data = {}
+    animal_names = animals.keys()
+
+    for name in animal_names:
+        health_data[name] = HEALTH[random.randint(0, len(HEALTH)-1)]
+    
+    return health_data
+
+
+def generate_feeding_logs(animals, carnivorous_foods, herbivorous_foods) -> dict:
+    # thinking we could generate roughly a week's worth of logs, 3 meals a day, 7 days
+    # would incorporate some pseudo random timing such that feeding isnt always the same time each day
+    # need also to include the food each animal eats
+
+    feeding_data = {}
+    animal_names = animals.keys()
+
+    breakfast_hours = [6, 7, 8, 9]
+    lunch_hours = [12, 13, 14, 15]
+    dinner_hours = [19, 20, 21, 22]
+
+    month = datetime.datetime.now().month
+    end_day = datetime.datetime.now().day
+    year = datetime.datetime.now().year
+
+    feeding_dates = []
+
+    for k in range(8):
+
+        # k should increment from 0 to 7
+        day = end_day - k
+
+        if day == 0:
+            month -= 1
+
+            if month in [9, 4, 6, 11]:
+                day = 30
+            elif month == 2 and year % 4 == 0:
+                day = 29
+            elif month == 2:
+                day = 28
+            else:
+                day = 31
+
+        feeding_dates.append(f'{year}/{month}/{day}')   # working as expected so far
+
+    feeding_dates.reverse()
+
+    for name in animal_names:
+        # need to generate the most recent week of feeding
+        # lets end with today's date for fun :)
+
+        diet = animals[name]['diet']
+        food_choices = None
+        
+        breakfasts = {}
+        lunches = {}
+        dinners = {}
+
+        for day in feeding_dates:
+            rand_minute = random.randint(0, 59)
+            if rand_minute < 10:
+                rand_minute = f'0{rand_minute}'
+
+            if diet == 'carnivore':
+                food_choices = carnivorous_foods
+            elif diet == 'herbivorous':
+                food_choices = herbivorous_foods
+            else:
+                food_choices = carnivorous_foods + herbivorous_foods
+
+
+            breakfasts[f'{day}-{breakfast_hours[random.randint(0, 3)]}:{rand_minute}'] = food_choices[random.randint(0, len(food_choices)-1)]
+            lunches[f'{day}-{lunch_hours[random.randint(0, 3)]}:{rand_minute}'] = food_choices[random.randint(0, len(food_choices)-1)]
+            dinners[f'{day}-{dinner_hours[random.randint(0, 3)]}:{rand_minute}'] = food_choices[random.randint(0, len(food_choices)-1)]
+
+        feeding_data[name] = {
+            'breakfasts': breakfasts,
+            'lunches': lunches,
+            'dinners': dinners
+        }
+
+    return feeding_data
---
+++ markell-python/project/zoo.py
@@ -0,0 +1,105 @@
+'''
+Zoo Project Layout:
+
+Example structure given was to log the times of day the animals get fed, what type of food they eat, medical conditions, etc.
+
+
+Required code usage:
+
+1.  multi line comment
+2.  1 or more constants                                     done
+3.  3 or more user defined functions                        done
+4.  1 or more loops (for or while, doesn't matter)          done
+5.  1 or more conditional blocks (if, elif, else)           done
+6.  use of file handling                                    done
+7.  Dictionary OR                                           done
+    List/Tuple(+5 or more manipulations) OR 
+    Set(+5 or more manipulations)
+
+
+Data Structuring Example:
+
+Zoo_data = {
+
+    'Animals': {
+        'name': {
+            species: _, 
+            age: _, 
+            personality: _,
+            diet: _
+        }
+    },
+
+    
+    '7-Day Feeding Logs': {
+        'name': {
+            breakfasts: {times/dates: food eaten}, 
+            lunches: {times/dates: food eaten}, 
+            dinners: {times/dates: food eaten}
+        }
+    },
+
+    
+    'Medical Conditions': {
+        'name': 'medical state'
+    }
+
+}
+
+'''
+
+from helper_functions import *
+
+# Let's start by creating a list of what animals should be included within our zoo :)
+zoo_animals_species = ['bear', 'lion', 'tiger', 'snake', 'zebra', 'giraffe', 'hippo', 'lizard', 'fish', 'panda', 'goat', 'camel', 'tortoise', 'sea turtle', 'whale', 'panther', 'elephant', 'rhino', 'flamingo', 'monkey', 'owl']
+
+
+# We can also create some subclasses of food preference of each animal
+diet = ['carnivore', 'herbivore', 'omnivore']
+carnivorous_foods = ['veal', 'chicken', 'fish']
+herbivorous_foods = ['plants', 'leaves', 'grass']
+
+# Now we need a list of animal names (sourced in animal-names.txt file created from https://www.bluecross.org.uk/sites/default/files/d8/downloads/Blue-Cross-top-100-pet-dog-names.pdf)
+common_animal_names = None
+with open('animal-names.txt', 'r') as file:
+    common_animal_names = file.readline().split(' ')
+
+
+# generate a list of common animal health conditions, favoring healthy (health-condition.txt file created from https://www.ardmoreah.com/resources/pet-care/common-pet-health-issues/)
+animal_health_conditions = None
+with open('health-conditions.txt', 'r') as file:
+    animal_health_conditions = file.readline().split(',')
+for k in range(20):
+    animal_health_conditions.append('healthy')
+
+
+# generate a list of possible animal personalities
+animal_personalities = None
+with open('personalities.txt', 'r') as file:
+    animal_personalities = file.readline().split(' ')
+
+# Define the size of our zoo animal population
+POPULATION = 5
+
+
+# Randomize some pairs of animals and names to create unique, non repeating entries for our database dictionary
+Animals = generate_zoo_population(POPULATION, zoo_animals_species, common_animal_names, animal_personalities, diet)
+print(f'Animal Info:\n{Animals}\n\n')
+
+
+# create a new dict to store the health conditions of each animal
+Health_data = generate_health_data(Animals, animal_health_conditions)
+print(f'Animal Health:\n{Health_data}\n\n')
+
+
+# create another new dict to hold data of the 7 day feeding log
+Feed_data = generate_feeding_logs(Animals, carnivorous_foods, herbivorous_foods)
+print(f'Animal Feed:\n{Feed_data}\n\n')
+
+
+# clump all data together like so, not really sure why we need this but oh well
+Zoo_data = {
+    'Animals': Animals,
+    '7-Day Feed Log': Feed_data,
+    'Health Status': Health_data
+}
